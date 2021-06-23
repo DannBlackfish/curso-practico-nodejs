66f360c3bc141c88e1055988f15d7d9b015fe133
DannBlackfish <monsterjunior@outlook.es>
2021-06-23 16:10:10 -0700

    register auth

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -5,23 +5,41 @@ const Controller = require('./index')
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    Controller.list()
-    .then((lista) => {
-        response.succes(req, res, lista, 200)
-    })
-    .catch((err) => {
-        response.error(req, res, err.message, 500)
-    })
-    
-})
+//Routes
+router.get('/', list);
+router.get('/:id', get);
+router.post('/', upsert);
+router.put('/', upsert);
 
-router.get('/:id', function(req, res) {
+//Internal functions
+function list(req, res) {
+    Controller.list()
+        .then((lista) => {
+            response.success(req, res, lista, 200);
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+}
+
+function get (req, res) {
     Controller.get(req.params.id)
-    .then((user) => {
-        response.success(req, res, user, 200)
-    });
-    response.succes(req, res, lista, 200)
-})
+        .then((user) => {
+            response.success(req, res, user, 200)
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500)
+        });
+}
+
+function upsert(req, res) {
+    Controller.upsert(req.body)
+        .then((user) => {
+            response.success(req, res, user, 201)
+        })
+        .catch((err) => {
+            response.error(req, res, err.message, 500);
+        });
+}
 
 module.exports = router
