dd8b2aef5b4c6e7ec5a281760a58088a56d204ad
DannBlackfish <monsterjunior@outlook.es>
2021-06-23 12:28:10 -0700

    Isolate code from db

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const response = require('../../../network/response')
+const response = require('../../../network/response');
+const Controller = require('./controller')
 
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    response.succes(req, res, 'Correctly', 200)
+    const lista = Controller.list();
+    response.succes(req, res, lista, 200)
 })
 
 module.exports = router
