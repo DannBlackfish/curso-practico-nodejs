a7ffb8d86876770e9a417440a6c034c5ec235daa
DannBlackfish <monsterjunior@outlook.es>
2021-06-23 18:41:00 -0700

    auth login

--- network/response.js
+++ network/response.js
@@ -1,6 +1,7 @@
 exports.succes = function(req, res, message, status) {
     let statusCode = status || 200;
     let statusMessage = message || '';
+    
     res.status(status).send({
         error: false, 
         status: status,
